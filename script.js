9df7f6647b38741f9f5e4cb7e24e5da185f0dbda
MAICA TRUTNER KOBRYNIEC <49192000@ort>
2025-09-04 08:38:52 -0300

    mmm

--- script.js
+++ script.js
@@ -1,10 +1,36 @@
-function Escribir(valor){
+let operadores = ['+', '-', '*', '/'];
+let ListaNumeros = [];
+let ListaOperadores = [];
+function EscribirNumero(valor){
     const operacion = document.getElementById('operacion');
+    if(operadores.includes(operacion.innerHTML[operacion.innerHTML.lenght - 1]))
+    {
+        ListaNumeros.push(valor);
+    }else
+    {
+        ListaNumeros[ListaNumros.lenght-1] += valor;
+    }
     operacion.innerHTML += valor;
+}
+function EscribirOperador(valor){
+    const operacion = document.getElementById('operacion');
+    if(!(operadores.includes(operacion.innerHTML[operacion.innerHTML.lenght - 1])))
+    {
+        if(ListaOperadores.length < 1)
+        {
+            ListaOperadores.push(valor);
+            operacion.innerHTML += valor;
+        }else
+        {
+            Calcular();
+            
+        }
+    }
 }
 function BorrarUltimo(){
     let operacion = document.getElementById('operacion');
     operacion.innerHTML = operacion.innerHTML.slice(0, -1);
+    ListaElementos.pop();
 }
 function Calcular(){
     let expresion = document.getElementById('operacion').innerHTML;
